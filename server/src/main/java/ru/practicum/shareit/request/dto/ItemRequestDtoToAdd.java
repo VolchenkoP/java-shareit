23f50ffcd6af9f5b0020fde68b591f4633b442d9
Pavel Volchenko <paulvolchenko@gmail.com>
2024-09-29 22:48:28 +0300

    Добавить тесты3

--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoToAdd.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoToAdd.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
--- server/src/test/java/ru/practicum/shareit/request/ItemRequestServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/request/ItemRequestServiceImplTest.java
@@ -1,92 +1,121 @@
-package ru.practicum.shareit.request;
+package ru.practicum.shareit.request.service;
 
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestDtoToAdd;
 import ru.practicum.shareit.request.dto.ItemRequestResponseDto;
 import ru.practicum.shareit.request.model.ItemRequest;
-import ru.practicum.shareit.request.repository.ItemRequestRepository;
-import ru.practicum.shareit.request.service.ItemRequestServiceImpl;
-import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.user.repository.UserRepository;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserService;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
-@ExtendWith(MockitoExtension.class)
+
+@Transactional
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class ItemRequestServiceImplTest {
-    private final User user = User.builder()
-            .id(1L)
-            .name("Oleg Gazmanov")
-            .email("vpole.skonem@viydu.ru")
-            .build();
-    @Mock
-    ItemRequestRepository itemRequestRepository;
-    @Mock
-    UserRepository userRepository;
-    @InjectMocks
-    ItemRequestServiceImpl itemRequestService;
-    private ItemRequest request;
+    private final EntityManager em;
+    private final ItemRequestService service;
+    private final UserService userService;
 
-    @BeforeEach
-    void setUp() {
-        request = new ItemRequest();
-        request.setId(1L);
-        request.setRequester(user);
-        request.setDescription("Description");
-        request.setCreated(LocalDateTime.now());
+    @Test
+    @DisplayName(value = "Добавление запроса на позицию")
+    void addItemRequestTest() {
+        ItemRequestDtoToAdd itemRequestDto = new ItemRequestDtoToAdd();
+        itemRequestDto.setDescription("desc");
+        UserDto userDto = userService.create(UserDto.builder()
+                .name("user")
+                .email("user@mail.ru")
+                .build());
+        ItemRequestDto result = service.addItemRequest(userDto.getId(), itemRequestDto);
+
+        TypedQuery<ItemRequest> query = em.createQuery("Select u from ItemRequest u where u.id = :id", ItemRequest.class);
+        ItemRequest itemRequest = query.setParameter("id", result.getId())
+                .getSingleResult();
+
+        assertThat(itemRequest.getId(), notNullValue());
+        assertThat(itemRequest.getDescription(), equalTo(itemRequestDto.getDescription()));
+        assertThat(itemRequest.getRequester().getId(), equalTo(userDto.getId()));
     }
 
     @Test
-    void addItemRequestSuccessful() {
-        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
-        when(itemRequestRepository.save(any(ItemRequest.class))).thenReturn(request);
-        ItemRequestDtoToAdd addDto = new ItemRequestDtoToAdd();
-        addDto.setDescription("Description");
-        ItemRequestDto dto = itemRequestService.addItemRequest(user.getId(), addDto);
-        assertEquals(request.getId(), dto.getId());
+    @DisplayName(value = "Получение всех запросов")
+    void getAllItemRequestsTest() {
+        UserDto userDto = userService.create(UserDto.builder()
+                .name("user")
+                .email("user@mail.ru")
+                .build());
+        ItemRequestDtoToAdd itemRequestDto = new ItemRequestDtoToAdd();
+        itemRequestDto.setDescription("new item request");
+        ItemRequestDtoToAdd itemRequestDtoAnother = new ItemRequestDtoToAdd();
+        itemRequestDtoAnother.setDescription("another item request");
+        ItemRequestDtoToAdd itemRequestDtoThird = new ItemRequestDtoToAdd();
+        itemRequestDtoThird.setDescription("third item request");
+        service.addItemRequest(userDto.getId(), itemRequestDto);
+        service.addItemRequest(userDto.getId(), itemRequestDtoAnother);
+        service.addItemRequest(userDto.getId(), itemRequestDtoThird);
+
+        Collection<ItemRequestResponseDto> result = service.findRequestByUserId(userDto.getId());
+        assertThat(result, hasSize(3));
     }
 
     @Test
-    void getItemRequestsUserHasRequests() {
-        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
-        when(itemRequestRepository.findByRequesterIdOrderByCreatedDesc(anyLong())).thenReturn(List.of(request));
-        List<ItemRequestResponseDto> dtos = itemRequestService.findRequestByUserId(user.getId());
-        assertEquals(1, dtos.size());
-        assertEquals(request.getId(), dtos.get(0).getId());
+    @DisplayName(value = "Получение запросов по ид пользователя")
+    void getItemRequestsByRequestorIdTest() {
+        UserDto userDto = userService.create(UserDto.builder()
+                .name("user")
+                .email("user@mail.ru")
+                .build());
+        UserDto userDtoAnother = userService.create(UserDto.builder()
+                .name("user2")
+                .email("user2@mail.ru")
+                .build());
+        ItemRequestDtoToAdd itemRequestDto = new ItemRequestDtoToAdd();
+        itemRequestDto.setDescription("new item request");
+        ItemRequestDtoToAdd itemRequestDtoAnother = new ItemRequestDtoToAdd();
+        itemRequestDtoAnother.setDescription("another item request");
+
+        service.addItemRequest(userDto.getId(), itemRequestDto);
+        service.addItemRequest(userDtoAnother.getId(), itemRequestDtoAnother);
+
+        List<ItemRequestResponseDto> result = service.findRequestByUserId(userDto.getId());
+        assertThat(result, hasSize(1));
+        assertThat(result.getFirst().getDescription(), is(itemRequestDto.getDescription()));
+        List<ItemRequestResponseDto> resultAnother = service.findRequestByUserId(userDtoAnother.getId());
+        assertThat(resultAnother, hasSize(1));
+        assertThat(resultAnother.getFirst().getDescription(), is(itemRequestDtoAnother.getDescription()));
     }
 
     @Test
-    void getAllItemRequestsSuccessful() {
-        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
-        PageRequest pageRequest = PageRequest.of(1, 20, Sort.by(Sort.Direction.DESC, "created"));
-        Page<ItemRequest> page = new PageImpl<>(List.of(request), pageRequest, 1);
-        when(itemRequestRepository.findAll(pageRequest)).thenReturn(page);
-        List<ItemRequestDto> dtos = itemRequestService.findAllWithParamsFromAndSize(user.getId(), 1, 20);
-        assertEquals(1, dtos.size());
-        assertEquals(request.getId(), dtos.getFirst().getId());
+    @DisplayName(value = "Получение отдельного запроса ИД")
+    void getItemRequestByIdTest() {
+        UserDto userDto = userService.create(UserDto.builder()
+                .name("user")
+                .email("user@mail.ru")
+                .build());
+        ItemRequestDtoToAdd itemRequestDto = new ItemRequestDtoToAdd();
+        itemRequestDto.setDescription("new item request");
+
+        ItemRequestDto addedItemRequest = service.addItemRequest(userDto.getId(), itemRequestDto);
+        ItemRequestResponseDto result = service.findItemRequestById(userDto.getId(), addedItemRequest.getId());
+
+        assertThat(result.getId(), notNullValue());
+        assertThat(result.getDescription(), equalTo(itemRequestDto.getDescription()));
+        assertThat(result.getId(), equalTo(addedItemRequest.getId()));
     }
 
-    @Test
-    void getItemRequestRequestExists() {
-        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
-        when(itemRequestRepository.findById(request.getId())).thenReturn(Optional.of(request));
-        ItemRequestResponseDto dto = itemRequestService.findItemRequestById(user.getId(), request.getId());
-        assertEquals(request.getId(), dto.getId());
-    }
 }
